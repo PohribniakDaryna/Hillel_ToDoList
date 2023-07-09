c988ad9d96e2f7b433c91431ad31d4d4556635ce
PohribniakDaryna <dpogribnyak@ukr.net>
2023-07-09 14:26:44 +0300

    Added HW 14 EF Core (Sqlite). Created the logic  "controller-service-repository" for TaskItem

---
+++ ToDoList/Services/TaskService.cs
@@ -0,0 +1,59 @@
+﻿using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly ITaskRepository register;
+        public TaskService(ITaskRepository register)
+        {
+            this.register = register;
+        }
+
+        public ITaskItem? GetTaskById(int id)
+        {
+            return register.GetTaskById(id);
+        }
+
+        public void AddTask(CreateTaskItemRequest request)
+        {
+            TaskItem task = new()
+            {
+                Id = register.GetTasks().Count+1,
+                Title = request.Title,
+                DeadLine = request.DeadLine,
+                Description = request.Description
+            };
+            register.AddTask(task);
+        }
+
+        public bool DeleteTask(int id)
+        {
+            int count = register.GetTasks().Count;
+            var task = register.GetTaskById(id);
+            if (task == null) return false;
+            register.DeleteTask(id);
+            int countAfterDeleting = register.GetTasks().Count;
+            if (countAfterDeleting >= count) return false;
+            return true;
+        }
+
+        public ITaskItem? UpdateTask(int id, CreateTaskItemRequest request)
+        {
+            var task = register.GetTaskById(id);
+            if (task != null)
+            {
+                task.Title = request.Title;
+                task.DeadLine = request.DeadLine;
+                task.Description = request.Description;
+            }
+            register.UpdateTask(id, (TaskItem)task);
+            return task;
+        }
+
+        public List<TaskItem> GetTasks()
+        {
+            return register.GetTasks();
+        }
+    }
+}
